f93f0309144d5b001bed31795db54c660f6b2265
Danny Oka <dannyoka@gmail.com>
2025-10-27 15:44:33 -0600

    updates

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-from internal.api.jira_client import JiraClient
-from textual.app import App
-from internal.views.Dashboard import Dashboard
+from internal.app import JiraTUI
 import logging
 
 logging.basicConfig(filename="app.log", level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-
-
-class JiraTUI(App):
-    def on_mount(self):
-        self.push_screen(Dashboard())
-        self.jira_client = JiraClient()
-
 
 if __name__ == "__main__":
     JiraTUI().run()
